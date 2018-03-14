09cabbaa1cd5afd366f53ccae680ea37844b348e
Maximo H <maxmonteiro86@gmail.com>
2018-03-14 14:25:10 -0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <pagina tamanho="10">
+        <painel titulo="Dashboard">
+            <p>Teste de Conteúdo</p>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <caixa qtd="80" titulo="Artigos" url="#teste" cor="orange" icone="ion ion-pie-graph"></caixa>
+                </div>
+
+                <div class="col-md-4">
+                    <caixa qtd="1500" titulo="Usuários" url="#" cor="blue" icone="ion ion-person-stalker"></caixa>
+                </div>
+
+                <div class="col-md-4">
+                    <caixa qtd="5" titulo="Autores" url="#" cor="red" icone="ion ion-person"></caixa>
+                </div>
+            </div>
+
+
+        </painel>
+    </pagina>
+@endsection
+
+
