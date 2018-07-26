e92722e93d96ca350393f76bda712943368bc589
Maximo Henrique <maxmonteiro86@gmail.com>
2018-07-26 10:03:42 -0300

    Criado formulario de inclusao

--- public/js/app.js
+++ public/js/app.js
@@ -1382,7 +1382,7 @@ function applyToTag (styleElement, obj) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(13);
-module.exports = __webpack_require__(70);
+module.exports = __webpack_require__(73);
 
 
 /***/ }),
@@ -1415,6 +1415,7 @@ Vue.component('tabela-lista', __webpack_require__(58));
 Vue.component('migalhas', __webpack_require__(61));
 Vue.component('modal', __webpack_require__(64));
 Vue.component('modal-link', __webpack_require__(67));
+Vue.component('formulario', __webpack_require__(70));
 
 var app = new Vue({
   el: '#app'
@@ -44797,6 +44798,142 @@ if (false) {
 
 /***/ }),
 /* 70 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+var normalizeComponent = __webpack_require__(1)
+/* script */
+var __vue_script__ = __webpack_require__(71)
+/* template */
+var __vue_template__ = __webpack_require__(72)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/assets/js/components/Formulario.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-24dfab2e", Component.options)
+  } else {
+    hotAPI.reload("data-v-24dfab2e", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 71 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    props: ['css', 'action', 'method', 'enctype', 'token'],
+    data: function data() {
+        return {
+            alterMethod: ''
+        };
+    },
+    computed: {
+        defineMethod: function defineMethod() {
+            if (this.method.toLowerCase() == "post" || this.method.toLowerCase() == "get") {
+                return this.method.toLowerCase();
+            }
+
+            if (this.method.toLowerCase() == "put") {
+                this.alterMethod = "put";
+            }
+
+            if (this.method.toLowerCase() == "delete") {
+                this.alterMethod = "delete";
+            }
+
+            return "post";
+        }
+    }
+});
+
+/***/ }),
+/* 72 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "form",
+    {
+      class: _vm.css,
+      attrs: {
+        action: _vm.action,
+        method: _vm.defineMethod,
+        enctype: _vm.enctype
+      }
+    },
+    [
+      _vm.alterMethod
+        ? _c("input", {
+            attrs: { type: "hidden", name: "_method", value: "alterMethod" }
+          })
+        : _vm._e(),
+      _vm._v(" "),
+      _vm.token
+        ? _c("input", {
+            attrs: { type: "hidden", name: "_token" },
+            domProps: { value: _vm.token }
+          })
+        : _vm._e(),
+      _vm._v(" "),
+      _vm._t("default")
+    ],
+    2
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-24dfab2e", module.exports)
+  }
+}
+
+/***/ }),
+/* 73 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -19,44 +19,18 @@
 
     <!-- Modal component -->
     <modal nome="meuModalTeste">
-        <painel titulo="Modal">
-            <div class="form-group has-success">
-                <label class="control-label" for="inputSuccess1">Input with success</label>
-                <input type="text" class="form-control" id="inputSuccess1" aria-describedby="helpBlock2">
-                <span id="helpBlock2" class="help-block">A block of help text that breaks onto a new line and may extend beyond one line.</span>
-            </div>
-            <div class="form-group has-warning">
-                <label class="control-label" for="inputWarning1">Input with warning</label>
-                <input type="text" class="form-control" id="inputWarning1">
-            </div>
-            <div class="form-group has-error">
-                <label class="control-label" for="inputError1">Input with error</label>
-                <input type="text" class="form-control" id="inputError1">
-            </div>
-            <div class="has-success">
-                <div class="checkbox">
-                <label>
-                    <input type="checkbox" id="checkboxSuccess" value="option1">
-                    Checkbox with success
-                </label>
+        <painel titulo="Adicionar">
+            <formulario css="" action="#" method="post" ecntype="" token="12345">
+                <div class="form-group">
+                    <label for="titulo">Título</label>
+                    <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
                 </div>
-            </div>
-            <div class="has-warning">
-                <div class="checkbox">
-                <label>
-                    <input type="checkbox" id="checkboxWarning" value="option1">
-                    Checkbox with warning
-                </label>
+                <div class="form-group">
+                    <label for="descricao">Descrição</label>
+                    <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
                 </div>
-            </div>
-            <div class="has-error">
-                <div class="checkbox">
-                <label>
-                    <input type="checkbox" id="checkboxError" value="option1">
-                    Checkbox with error
-                </label>
-                </div>
-            </div>
+                <button class="btn btn-info">Adicionar</button>
+            </formulario>
         </painel>
 
     </modal>
